73cadf182c272c3ade06537d676b1ca5b9cb2979
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2023-04-10 15:25:10 -0400

    Changes: - The model name MenuItem is changed to Menu. - Functionality is added to test-views.

--- littlelemon/tests/test-models.py
+++ littlelemon/tests/test-models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from restaurant.models import MenuItem
+from restaurant.models import Menu
 
 class MenuTest(TestCase):
     def test_get_item(self):
-        item = MenuItem.objects.create(title='Icecream', price=80, inventory=100)
+        item = Menu.objects.create(title='Icecream', price=80, inventory=100)
         self.assertEqual(item, 'Icecream: 80')
