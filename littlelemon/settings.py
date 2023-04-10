3c24d2a196a03c542950fe3486b29d67571812bf
Cristian Araujo <cristianaraujoaraujo@gmail.com>
2023-04-10 15:26:24 -0400

    Settings Added: - Configurations for Rest Framework are added. - Configurations for Djoser are added.

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
 
@@ -133,3 +134,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django Rest Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+# Djoser settings
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
